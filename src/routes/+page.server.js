4b00261c272584835dd6ff300f79da5270fc53e8
Daniel Kostuj <Dkostuj@gmx.de>
2025-09-25 23:13:05 +0200

    Modify the load() request

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,20 +1,23 @@
 import { supabase } from '$lib/supabaseClient';
 import { SUPABASE_TABLE_NAME } from '$env/static/private';
 
+export const prerender = false;
+
 export async function load({setHeaders}) {
     setHeaders({
-		'cache-control': 'no-store'
-	});
-    let newData;
+        'cache-control': 'no-cache, no-store, must-revalidate',
+        pragma: 'no-cache',
+        expires: '0'
+    });
+
 	const { data } = await supabase
 		.from(SUPABASE_TABLE_NAME)
 		.select()
 		.order('points', { ascending: false })
 		.order('created_at', { ascending: false });
 
-    newData = data;
 	return {
-		leaderboardEntries: newData || [],
+		leaderboardEntries: data || [],
         createdAt: new Date()
 	};
 }
